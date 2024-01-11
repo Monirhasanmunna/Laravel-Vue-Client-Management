472e7703c127aaf402a7f5c166ad9e25bc77b783
MH Munna <monirhasanmunna1@gmail.com>
2024-01-11 16:45:52 +0600

    header auth user email show

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,8 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return Inertia::render('Auth/Login');
+})->middleware('guest');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
